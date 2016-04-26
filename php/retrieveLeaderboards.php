8d22de33cbf8d780a763efef3a341e1ea9a7e10a
Desember29 <thomas.desember29@gmail.com>
2016-04-26 23:01:53 +0200

    Made changes to how the SQL is parameterized
    Made changes to how the SQL is parameterized

--- php/retrieveLeaderboards.php
+++ php/retrieveLeaderboards.php
@@ -12,8 +12,12 @@
 		
 		//Check that leaderboard_mode has been set
 		if (!empty($_GET["leaderboard_mode"])) {
-			$sqlRetrieveLeaderboard = "
-					SELECT HH.username, R.rank_image, SUM(HS.value) as score
+			$sqlRetrieveLeaderboard = null;
+
+			if($_GET["leaderboard_mode"] === "total") {
+				echo "Enters total!";
+				$sqlRetrieveLeaderboard = "
+					SELECT HH.username, R.rank_image, HS.value as score
 					FROM (
 						SELECT MAX(HR.rank_rank_id) AS householdMaxRank, HR.household_household_id
 						FROM household_ranks AS HR
@@ -23,31 +27,41 @@
 					INNER JOIN household AS HH ON maxRank.household_household_id = HH.household_id
 					INNER JOIN rank AS R ON maxRank.householdMaxRank = R.rank_id
 					INNER JOIN household_scores AS HS ON HH.household_id = HS.household_household_id
-					WHERE :scoreType HS.score_type_score_type_id = 0
-					:date
+					WHERE HS.score_type_score_type_id = 0
 					GROUP BY HH.household_id
 					ORDER BY HS.value DESC
 					";
-			$retrieveLeaderboard = $dbh->prepare($sqlRetrieveLeaderboard);
-			if($_GET["leaderboard_mode"] === "total") {
-				echo "Enters total!";
-				$retrieveLeaderboard->bindParam(":scoreType", $scoreType = "", PDO::PARAM_STR);
-				$retrieveLeaderboard->bindParam(":date", $date = "", PDO::PARAM_STR);
-				echo "Parameters should have been binded!";
+				$retrieveLeaderboard = $dbh->prepare($sqlRetrieveLeaderboard);
 				echo $sqlRetrieveLeaderboard;
 			}
 			elseif($_GET["leaderboard_mode"] === "timed") {
 				echo "Enters timed!";
 				if(!empty($_GET["start_date"]) && !empty($_GET["end_date"])) {
-					$retrieveLeaderboard->bindParam(":scoreType", $scoreType = "NOT", PDO::PARAM_STR);
-					$retrieveLeaderboard->bindParam(":date", $date = "AND HS.date BETWEEN '".$_GET["start_date"]."' AND '".$_GET["end_date"]."'" , PDO::PARAM_STR);
+					$sqlRetrieveLeaderboard = "
+						SELECT HH.username, R.rank_image, SUM(HS.value) as score
+						FROM (
+							SELECT MAX(HR.rank_rank_id) AS householdMaxRank, HR.household_household_id
+							FROM household_ranks AS HR
+							WHERE HR.date_obtained IS NOT NULL
+							GROUP BY HR.household_household_id
+						) AS maxRank
+						INNER JOIN household AS HH ON maxRank.household_household_id = HH.household_id
+						INNER JOIN rank AS R ON maxRank.householdMaxRank = R.rank_id
+						INNER JOIN household_scores AS HS ON HH.household_id = HS.household_household_id
+						WHERE NOT HS.score_type_score_type_id = 0
+						AND HS.date BETWEEN :startDate AND :endDate
+						GROUP BY HH.household_id
+						ORDER BY HS.value DESC
+						";
+					$retrieveLeaderboard = $dbh->prepare($sqlRetrieveLeaderboard);
+					$retrieveLeaderboard->bindParam(":startDate", $_GET["start_date"], PDO::PARAM_STR);
+					$retrieveLeaderboard->bindParam(":endDate",  ,$_GET["end_date"] PDO::PARAM_STR);
 					echo "Parameters should have been binded!";
 					echo $sqlRetrieveLeaderboard;
 				} else {
-					echo "Need the date to and from to retrieve leaderboards from a timespan!";
+					echo "Need the start_date and end_date to retrieve leaderboards from a timespan!";
 				}
 			}
-			echo $sqlRetrieveLeaderboard;
 			$retrieveLeaderboard->execute();
 			$leaderboard = $retrieveLeaderboard->fetchAll(PDO::FETCH_ASSOC);
 			echo $jsonLeaderboard = json_encode($leaderboard);
