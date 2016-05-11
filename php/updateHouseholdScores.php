3729a67f88180d4d501db42e0394c02a71b9636e
Desember29 <thomas.desember29@gmail.com>
2016-05-11 14:42:45 +0200

    updateHouseholdScores.php is almost finished
    updateHouseholdScores.php is almost finished. Have completed most parts
    of it, but it is missing the fetching of power throughput data.

--- php/updateHouseholdScores.php
+++ php/updateHouseholdScores.php
@@ -11,87 +11,90 @@
 		
 		
 		if (isset($_GET["household_id"])) {
-			//Note: Any change in database score types or order must be done here aswell.
+			//NOTE: Any change in database score types or order must be done here aswell.
 			//Is used to check for score types and insert them into the database.
-			$scoreTypeKeys = array("Total Score", "PV Score", "Grid Score", "Scheduling Score", "Share Score");
-			$scoreType = array(0,1,2,3,4);
-			$scoreTypes = array_combine($scoreTypeKeys, $scoreType);
-			
+			$scoreTypes = array(0,1,2,3,4);
 			
 			//Is used as parameters in MySQL and DBO
 			$household_id = $_GET["household_id"];
-			$type = null;
-			$points = null;
-			$pointsArray = array_combine($scoreType, $scoreType);
+			$scoreType = null;
+			$score = null;
 			$startOfMonth = date("Y-m")."-01";
 			$startDate = null;
-			$endDate = date("Y-m-d");
 			
+			//The data on household throughput for the month
+			//TODO insert fetching method of data from clientside.
+			$consumption = null;
+			$production = null;
+			$shared = null;
+			$numberOfDevicesScheduled = null;
+			$totalNumberOfDevices = null;
 			
-			//Fetches the multipliers from the game.ini file
+			//Fetches the multipliers from the game.ini file which are used to keep the scores within the same ranges
 			$multipliers = parse_ini_file("/var/www/html/game.ini");
 			
-			
 			//Calculates and stores the different score amounts.
+			$pvScore = ($production/$consumption) * $multipliers["pv"];
+			$gridScore = (1/$consumption) * $multipliers["grid"];
+			$schedulingScore = ($numberOfDevicesScheduled/$totalNumberOfDevices)  * $multipliers["scheduling"];
+			$sharingScore = ($shared/$consumption) * $multipliers["sharing"];
+			$totalScore = $pvScore+$gridScore+$schedulingScore+$sharingScore;
+			$scores = array($totalScore, $pvScore, $gridScore, $schedulingScore, $sharingScore);
 			
 			
-			
-			//MySQL and DBO for checking if a score exists
+			//MySQL and DBO for checking if a score exists for a household
 			$sqlCheckIfHouseholdScoreExist = "
 			SELECT *
 			FROM household_scores AS HS
 			WHERE HS.household_household_id = :household_id
 			AND HS.score_type_score_type_id = :score_type_id
-			AND HS.date BETWEEN :startDate AND :endDate
+			AND HS.date BETWEEN :startDate AND CURDATE()
 			LIMIT 1
 			";
 			$checkIfHouseholdScoreExist = $dbh->prepare($sqlCheckIfHouseholdScoreExist);
 			$checkIfHouseholdScoreExist->bindParam(":household_id", $household_id, PDO::PARAM_INT);
-			$checkIfHouseholdScoreExist->bindParam(":score_type_id", $type, PDO::PARAM_INT);
+			$checkIfHouseholdScoreExist->bindParam(":score_type_id", $scoreType, PDO::PARAM_INT);
 			$checkIfHouseholdScoreExist->bindParam(":startDate", $startDate, PDO::PARAM_STR);
-			$checkIfHouseholdScoreExist->bindParam(":endDate", $endDate, PDO::PARAM_STR);
 			
 			
-			//MySQL and DBO for inserting missing household score types
+			//MySQL and DBO for inserting missing household score types for the household
 			$sqlInsertHouseholdScoreType = "
 			INSERT INTO household_scores(household_household_id, score_type_score_type_id, date, value)
-			VALUES (:household_id, :score_type_id, :date, :value)
+			VALUES (:household_id, :score_type_id, CURDATE(), 0)
 			";
 			$insertHouseholdScoreType = $dbh->prepare($sqlInsertHouseholdScoreType);
 			$insertHouseholdScoreType->bindParam(":household_id", $household_id, PDO::PARAM_INT);
-			$insertHouseholdScoreType->bindParam(":score_type_id", $type, PDO::PARAM_INT);
-			$insertHouseholdScoreType->bindParam(":date", $endDate, PDO::PARAM_STR);
-			$insertHouseholdScoreType->bindParam(":value", $amount = 0, PDO::PARAM_INT);
+			$insertHouseholdScoreType->bindParam(":score_type_id", $scoreType, PDO::PARAM_INT);
 			
 			
 			//MySQL and DBO for updating scores for the household
 			$sqlUpdateHouseholdScore = "
 			UPDATE household_scores
-			SET date = :date,
+			SET date = CURDATE(),
 			value = value + :value
 			WHERE household_household_id = :household_id
 			AND score_type_score_type_id = :score_type_id
-			AND date BETWEEN :startDate AND :endDate
+			AND date BETWEEN :startDate AND CURDATE()
 			";
 			$updateHouseholdScore = $dbh->prepare($sqlUpdateHouseholdScore);
-			$updateHouseholdScore->bindParam(":date", $endDate, PDO::PARAM_STR);
-			$updateHouseholdScore->bindParam(":value", $points, PDO::PARAM_INT);
+			$updateHouseholdScore->bindParam(":value", $score, PDO::PARAM_INT);
 			$updateHouseholdScore->bindParam(":household_id", $household_id, PDO::PARAM_INT);
-			$updateHouseholdScore->bindParam(":score_type_id", $type, PDO::PARAM_INT);
+			$updateHouseholdScore->bindParam(":score_type_id", $scoreType, PDO::PARAM_INT);
 			$updateHouseholdScore->bindParam(":startDate", $startDate, PDO::PARAM_STR);
-			$updateHouseholdScore->bindParam(":endDate", $endDate, PDO::PARAM_STR);
 			
 			
-			//Iterate over different household score types and check if each exists, and if not insert them into the table then update the score
-			foreach($scoreTypes as $key => $value) {
-				$type = $value;
-				if ($type == 0) {
+			//Iterate over different household score types and check if each exists. If they don't exist insert them into the table then update the score based on the values calculated.
+			foreach($scoreTypes as $value) {
+				$scoreType = $value;
+				if ($scoreType == 0) {
 					$startDate = "2010-01-01";
 					$checkHouseholdScoreExist->execute();
 					$householdScoreExist = $checkHouseholdScoreExist->fetchAll();
 					if (count($householdScoreExist) < 1) {
 						$insertHouseholdScoreType->execute();
 					}
+					$score = $scores[$scoreType];
+					$updateHouseholdScore->execute();
 				} else {
 					$startDate = $startOfMonth;
 					$checkHouseholdScorExist->execute();
@@ -99,19 +102,40 @@
 					if (count($householdScoreExist) < 1) {
 						$insertHouseholdScoreType->execute();
 					}
-					$points = $pointsArray[($type-1)];
+					$score = $scores[$scoreType];
 					$updateHouseholdScore->execute();
 				}
 			}
-			//Has to be run once afterwards to update the total score for the household
-			$type = 0;
-			$points = $totalPoints;
-			$startDate = "2010-01-01";
-			$updateHouseholdScore->execute();
+			
+			
+			/*NOTE: It is very important that the ranks have sufficient amount of points between requirements,
+			so that a household doesn't gain more than one rank per score update.
+			This is because the query below picks out the highest rank_id that the household has not obtained and sets it so that the household has obtained it.
+			The problem occurs if there are two ranks that the household has obtained for one month as only the highest rank actually gets updated in the database,
+			while the lower will still stand as not achieved.
+			The subquery will also only pick the rank_id that is the highest rank_id that the household meets the requirement for, even if it has been obtained.*/
+			
+			//Updates the households rank if they meet the criteria for the next rank, after having updated their scores.
+			$sqlUpdateHouseholdRank = "
+			UPDATE household_ranks as HR
+			SET HR.date_obtained = CURDATE()
+			WHERE HR.household_household_id = :household_id
+			AND HR.date_obtained IS NULL
+			AND HR.rank_rank_id = (SELECT R.rank_id
+				FROM rank AS R
+				WHERE R.requirement <= (SELECT value
+					FROM household_scores AS HS
+					WHERE HS.household_household_id = :household_id
+					AND HS.score_type_score_type_id = 0)
+				ORDER BY R.rank_id DESC
+				LIMIT 0, 1)
+			";
+			$updateHouseholdRank = $dbh->prepare($sqlUpdateHouseholdRank);
+			$updateHouseholdRank->bindParam(":household_id", $household_id, PDO::PARAM_INT);
+			$updateHouseholdRank->execute();
 		} else {
 			echo "household_id must be set in order to update scores for the household!";
 		}
-
 		
 		
 		//Close connection
