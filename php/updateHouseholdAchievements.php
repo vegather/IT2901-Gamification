bbbb6e24bef22acb5661e900241e0c73ed85c073
lsnellingen <larssnellingen@hotmail.com>
2016-05-18 19:50:34 +0200

    Started on new achievemets
    It's not working right now

--- php/updateHouseholdAchievements.php
+++ php/updateHouseholdAchievements.php
@@ -11,31 +11,23 @@
 		
 		//Check if household_id has been set as a parameter
 		if (isset($_GET["household_id"])) {
+			
+			// Parameters for MySQL abd DBO
 			$household_id = $_GET["household_id"];
+			$startOfLastMonth = date("Y-n-d", strtotime("first day of previous month"));	
+			$endOftheLastMonth = date("Y-n-d", strtotime("last day of previous month"));			
 			
 			//MySQL for retrieving all the achievements that are not yet schieved for the household_id in question
 			$sqlRetrieveHouseholdNotAchievedAchievements = "
-			SELECT A.achievement_id
-			FROM achievement as A
-			INNER JOIN household_achievements AS HA ON A.achievement_id = HA.achievement_achievement_id
-			WHERE HA.household_household_id = :household_id
-			AND HA.achieved = 0
+				SELECT A.achievement_id
+				FROM achievement as A
+				INNER JOIN household_achievements AS HA ON A.achievement_id = HA.achievement_achievement_id
+				WHERE HA.household_household_id = :household_id
+				AND HA.achieved = 0
 			";
 			$sqlRetrieveHouseholdNotAchievedAchievements = $dbh->prepare($sqlRetrieveHouseholdAchievements);
 			$sqlRetrieveHouseholdNotAchievedAchievements->execute();
 			$householdNotAchieved = $sqlRetrieveHouseholdNotAchievedAchievements->fetchAll(PDO::FETCH_ASSOC);
-			
-			
-			//MySQL for retrieving totalscore for the household
-			$sqlRetrieveHouseholdTotalScore = "
-				SELECT value
-				FROM household_scores
-				WHERE score_type_score_type_id = :score_type_score_type_id
-				AND household_household_id = :household_id
-				";
-			$sqlRetrieveHouseholdTotalScore = $dbh->prepare($sqlRetrieveHouseholdTotalScore);
-			$sqlRetrieveHouseholdTotalScore->execute();
-			$householdTotalScore = $sqlRetrieveHouseholdTotalScore->fetchAll(PDO::FETCH_ASSOC);
 			
 			//MySQL for retrieving the date the household joined
 			$sqlRetrieveHouseholdJoined = "
@@ -46,31 +38,43 @@
 			$sqlRetrieveHouseholdJoined = $dbh->prepare($sqlRetrieveHouseholdJoined);
 			$sqlRetrieveHouseholdJoined->execute();
 			$householdJoined = $sqlRetrieveHouseholdJoined->fetchAll(PDO::FETCH_ASSOC);
-				
-			$value = null;
+			
 			//Checks if the user has been apart of the program for one quarter
-			if(in_arry($id = 4, $householdNotAchieved) $sqlRetrieveHouseholdJoined->add(new DateInterval('P3M') < date){
+			if(in_arry($id = 4, $householdNotAchieved) && $sqlRetrieveHouseholdJoined->add(new DateInterval('P3M') < date){
 				achievementAchived ($dbh , $id);
 			}
 			
 			//Checks if the user has been in the program for 1 year
-			if( in_arry($id = 5, $householdNotAchieved) $sqlRetrieveHouseholdJoined->add(new DateInterval('P1Y') < date){
+			if( in_arry($id = 5, $householdNotAchieved) && $sqlRetrieveHouseholdJoined->add(new DateInterval('P1Y') < date){
 				//MySQL and DBO for updating achieved achievemets
 				achievementAchived ($dbh , $id);
 			}
 			
 			//Checks if the achievement is in the householdNotAchieved array and if the user has the requirements to achieve it
-			if( in_arry($id = 7, $householdNotAchieved) && $householdTotalScore > 10000){
+			if( in_arry($id = 7, $householdNotAchieved) && getTotalscore($dbh) >= 10000){
 				achievementAchived ($dbh , $id);
 			}
 			
-			//MySQL and DBO for updating achieved achievemets
-			$sqlUpdateAchievedAchievements = "
-			UPDATE household_Achievents
-			SET date_achieved = CURDATE(),
-			AND achieved = 1
-			WHERE household_household_id = :household_id
-			";
+			if (in_arry($id = 1, $householdNotAchieved) && $sqlRetrieveHouseholdJoined->add(new DateInterval('P3M') < date){
+				$startOfSecondToLastMonth = date($startOfLastMonth, strtotime("-1 months"));	
+				$endOftheSecondToLastMonth = date($endOftheLastMonth, strtotime("-1 months"));	
+				scoreLastMonth = getScoreBetweenDates($dbh, $startOfLastMonth, $endOftheLastMonth);
+				scoreSecondToLastMonth = getScoreBetweenDates ($dbh, $startOfSecondToLastMonthLastMonth, $endOftheSecondToLastMonth);
+				if (scoreLastMonth > scoreSecondToLastMonth){
+					achievementAchived($dbh, $id);
+				}
+			}
+			
+			if (in_arry($id = 2, $householdNotAchieved) && $sqlRetrieveHouseholdJoined->add(new DateInterval('P7M') < date){
+				$startOfLastQuarter = date(startOfLastMonth, strtotime("-2 months"));	
+				$StartOfSecondToLastQuarter = date($startOfLastMonth, strtotime("-2 months"));
+				$endOftheSecondToLastQuarter = date($endOftheLastMonth, strtotime("-1 months"));
+				scoreLastMonth = getScoreBetweenDates($dbh, $endOftheLastMonth, $startOfLastQuarter);
+				scoreSecondToLastMonth = getScoreBetweenDates ($dbh, $startOfSecondToLastMonthLastMonth, $endOftheSecondToLastMonth);
+				if (scoreLastMonth > scoreSecondToLastMonth){
+					achievementAchived($dbh, $id);
+				}
+			}
 			
 			
 		} else {
@@ -97,6 +101,37 @@ function achievementAchived ($PDO , $achivement_ID){
 			$UpdateHouseholdAchievements->bindParam(":achievement_ID", $achivment_ID, PDO::PARAM_INT);
 			$UpdateHouseholdAchievemnts->execute();
 	}
+}
+
+//MySQL and DBO for getting score between two set dates
+function getScoreBetweenDates ($PDO, $startDate, $endDate){
+					$sqlRetrieveMonthScore = "
+						SELECT SUM(HS.value) as score
+						FROM household as HH
+						INNER JOIN household_scores AS HS ON HH.household_id = HS.household_household_id
+						WHERE NOT HS.score_type_score_type_id = 0
+						AND HS.date BETWEEN :startOfLastMonthDate AND :endOftheLastMonthDate
+					";
+				$retrieveMonthScore; = $dbh->prepare($sqlRetrieveMonthScore);
+				$retrieveMonthScore->bindParam(":startDate", $startDate, PDO::PARAM_STR);
+				$retrieveMonthScore->bindParam(":endDate",  $endDate, PDO::PARAM_STR);
+				$retrieveLeaderboard->execute();
+				$score = $retrieveLeaderboard->fetchAll(PDO::FETCH_ASSOC);
+				return $score
+}
+
+//MySQL and DBO for retrieving totalscore for the household
+function getTotalscore($PDO){
+			$sqlRetrieveHouseholdTotalScore = "
+					SELECT value
+					FROM household_scores
+					WHERE score_type_score_type_id = :score_type_score_type_id
+					AND household_household_id = :household_id
+				";
+			$sqlRetrieveHouseholdTotalScore = $dbh->prepare($sqlRetrieveHouseholdTotalScore);
+			$sqlRetrieveHouseholdTotalScore->execute();
+			$householdTotalScore = $sqlRetrieveHouseholdTotalScore->fetchAll(PDO::FETCH_ASSOC);
+			return $householdTotalScore
 }
 	
 ?>
