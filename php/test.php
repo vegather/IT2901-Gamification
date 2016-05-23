9dd7c8ecd6449587a186a8d46f3f4bdc939aabf2
Desember29 <thomas.desember29@gmail.com>
2016-05-23 12:51:51 +0200

    Added a test.php script to test certain code
    Added a test.php script to test certain code in the case the original
    script doesn't allow easy testing.

---
+++ php/test.php
@@ -0,0 +1,35 @@
+<?php
+	//Fetches connection information from the config.ini file then sets the connection variables
+	$iniArray = parse_ini_file("/var/www/html/config.ini", true);
+	$hostname = $iniArray["connectionInfo"]["hostname"];
+	$username = $iniArray["connectionInfo"]["username"];
+	$password = $iniArray["connectionInfo"]["password"];
+	$database = $iniArray["connectionInfo"]["database"];
+	
+	//Connection to the database
+	try {
+		$dbh = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+		
+		//Check if household_id has been set as a parameter
+		if (isset($_GET["household_id"])) {
+			$sqlRetrieveAchievementsID = "
+					SELECT achievement_id
+					FROM achievement
+					";
+			$retrieveAchievementsID = $dbh->prepare($sqlRetrieveAchievements);
+			$retrieveAchievementsID->execute();
+			$achievementsID = $retrieveAchievementsID->fetchAll(PDO::FETCH_NUM);
+			echo $achievementsID;
+			} else {
+			echo "You need to set household_id!";
+		}
+		
+		
+		//Close connection
+		$dbh = null;
+		
+		
+	} catch(PDOException $e) {
+		echo '<h1>An error has occured.</h1><pre>', $e->getMessage(), '</pre>';
+	}
+?>
